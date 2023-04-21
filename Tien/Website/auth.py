0fca126944da47cf74e11428c789ac6cc6cb9a26
Zero7645 <131488011+Zero7645@users.noreply.github.com>
2023-04-21 18:53:22 -0500

    Changed and added a few things from Tien's

--- Tien/Website/auth.py
+++ Tien/Website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, url_for, redirect
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
 
 auth = Blueprint('auth', __name__)
 
@@ -21,24 +25,61 @@ def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         print(email)
-        firstName = request.form.get('firstName')
-        print(firstName)
+        first_name = request.form.get('firstName')
+        print(first_name)
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
         if len(email) < 4:
             flash("Email must be greater than 4 character.", category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash("First name must be greater than 1 character.", category='error')
         elif password1 != password2:
             flash("Passwords don't match.", category='error')
         elif len(password1) < 7:
             flash("Password must be at least 7 character.", category='error')
         else:
-            # add user infor to database
-            # new_user = User(email=email, firstName=firstName, password=)
+            # add user info to database
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            # db.commit() #Causes Error - AttributeError: commit
             flash("Account created!", category='success')
-            pass
+            return redirect(url_for('my_view.home_page'))
 
     return render_template("sign-up.html")
 
+# Profile
+@auth.route('/profile', methods=['GET', 'POST'])
+def profile():
+    return render_template("profile.html", name='Zachary', email='bottleofwater@gmail.com', total_savings = '0.00', boolean=True)
+
+# Profile
+@auth.route('/rewards', methods=['GET', 'POST'])
+def rewards():
+    return render_template("rewards.html", boolean=True)
+
+# Profile
+@auth.route('/savings', methods=['GET', 'POST'])
+def savings():
+    if request.method == 'POST':
+        startTime = request.form.get('startTime')
+        print(startTime)
+        endTime = request.form.get('endTime')
+        print(endTime)
+        rate = request.form.get('rate')
+        
+        if len(startTime) < 0:
+            flash("Start time must be greater than 0.", category='error')
+        elif len(endTime) > 23:
+            flash("End time must be less than 23.", category='error')
+        elif startTime >= endTime:
+            flash("Start time needs to be less that end time.", category='error')
+        # else:
+        #     # add user info to database
+        #     new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+        #     db.session.add(new_user)
+        #     # db.commit() #Causes Error - AttributeError: commit
+        #     flash("Account created!", category='success')
+        #     return redirect(url_for('my_view.home_page'))
+
+    return render_template("savings.html", boolean=True)
