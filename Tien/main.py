81b9a46d20862ff15367a4127af318b8fe0af3af
TienMinhLN <115051978+TienMinhLN@users.noreply.github.com>
2023-04-22 07:18:53 -0500

    Update main.py

--- Tien/main.py
+++ Tien/main.py
@@ -8,13 +8,11 @@
 
 # import create_app function from the Website package
 from Website import create_app
-from flask import render_template
+
 
 app = create_app()
 
-@app.route('/map')
-def map():
-    return render_template('map.html')
+
 
 if __name__ == '__main__':
     # debug means everytime I change the code, it automatically rerun the web server
