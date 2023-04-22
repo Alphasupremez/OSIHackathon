03da7b6d02a4ff08ce6d3037db2834ad511ebf03
Zero7645 <131488011+Zero7645@users.noreply.github.com>
2023-04-21 19:55:24 -0500

    Slightly Updated

--- Tien/Website/auth.py
+++ Tien/Website/auth.py
@@ -20,13 +20,12 @@ def logout():
     return "<p>Logout</p>"
 
 
+# Signup
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        print(email)
         first_name = request.form.get('firstName')
-        print(first_name)
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
@@ -48,38 +47,57 @@ def sign_up():
 
     return render_template("sign-up.html")
 
+
+
+
+# Added stuff
+
+
 # Profile
 @auth.route('/profile', methods=['GET', 'POST'])
 def profile():
     return render_template("profile.html", name='Zachary', email='bottleofwater@gmail.com', total_savings = '0.00', boolean=True)
 
-# Profile
+# Rewards
 @auth.route('/rewards', methods=['GET', 'POST'])
 def rewards():
     return render_template("rewards.html", boolean=True)
 
-# Profile
+
+# Savings Function Test (Replace with Lor's)
+def saveFunction(start, end, rate):
+    return (start + end - rate)
+    
+
+# Savings
 @auth.route('/savings', methods=['GET', 'POST'])
 def savings():
+    # Default values
+    startTime = 0
+    endTime = 0
+    rate = 0
+    
+    # After pressing the submit button or enter this takes effect
     if request.method == 'POST':
         startTime = request.form.get('startTime')
-        print(startTime)
         endTime = request.form.get('endTime')
-        print(endTime)
         rate = request.form.get('rate')
         
-        if len(startTime) < 0:
+        # startTime needs to be 0-22
+        if int(startTime) < 0 or int(startTime) > 22:
             flash("Start time must be greater than 0.", category='error')
-        elif len(endTime) > 23:
+        
+        # endTime needs to be 1-23
+        elif int(endTime) > 23 or int(endTime) < 1:
             flash("End time must be less than 23.", category='error')
-        elif startTime >= endTime:
-            flash("Start time needs to be less that end time.", category='error')
-        # else:
-        #     # add user info to database
-        #     new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
-        #     db.session.add(new_user)
-        #     # db.commit() #Causes Error - AttributeError: commit
-        #     flash("Account created!", category='success')
-        #     return redirect(url_for('my_view.home_page'))
-
-    return render_template("savings.html", boolean=True)
+        
+        # startTime and endTime can't be equal to each other
+        elif int(startTime) >= int(endTime):
+            flash("Start time needs to be less than end time.", category='error')
+            
+        # Rate needs to be greater than 0
+        elif float(rate) <= 0:
+            flash("Rate needs to be greater than 0.", category='error')
+            
+    # formated way to pass the total_savings value from the function and immediatly display it
+    return render_template("savings.html", total_savings = f"{saveFunction(int(startTime), int(endTime), float(rate)) :0.2f}", boolean=True)
