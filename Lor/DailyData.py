068df6728425086dabbfc0f8339a198def24a48e
CLOR2003 <93290045+CLOR2003@users.noreply.github.com>
2023-04-22 11:00:19 -0500

    finished

--- Lor/DailyData.py
+++ Lor/DailyData.py
@@ -6,12 +6,13 @@ class DailyData:
         self.save_time_df = pd.DataFrame(columns=['StartTime', 'EndTime', 'Rate'])
         
     def calculate_rates(self):
-        self.energy_df = pd.read_csv('Jordan\\Energy.csv')
+        self.energy_df = pd.read_csv('Lor\\Energy.csv')
         for index, row in self.energy_df.iterrows():
             start_time = row['StartTime']
             end_time = row['EndTime']
             genlvl = row['GenLvl']
             genload = row['Generation'] - row['Load']
+            index1 = row['Index']
             if genload > 0:
                 if genlvl == 'low':
                     rate = 0.5
@@ -21,8 +22,8 @@ class DailyData:
                     rate = 0.3
             else:
                 rate = 0.6
-            save_row = pd.Series({'StartTime': start_time, 'EndTime': end_time, 'Rate': rate})
+            save_row = pd.Series({'Index': index1, 'StartTime': start_time, 'EndTime': end_time, 'Rate': rate})
             save_rows = save_row.to_frame().T
             self.save_time_df = pd.concat([self.save_time_df, save_rows], ignore_index=True)
-        self.save_time_df = self.save_time_df.drop(self.save_time_df.index[-1])
+        #self.save_time_df = self.save_time_df.drop(self.save_time_df.index[-1])
         return self.save_time_df
